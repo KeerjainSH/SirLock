e3e41f2b531bf7503aac7c4a6855075b9b5fc80f
Rafid Ferdianto <rafid.ferdianto@gmail.com>
2021-08-19 18:30:46 +0700

    fix ui/ux

--- sirlock/lib/services/dimension_service.dart
+++ sirlock/lib/services/dimension_service.dart
@@ -7,3 +7,10 @@ double getScreenWidth(BuildContext context, double percentage) {
 double getScreenHeight(BuildContext context, double percentage) {
   return MediaQuery.of(context).size.height * percentage / 100;
 }
+
+bool isVertical(BuildContext context) {
+  return (MediaQuery.of(context).size.height >
+          MediaQuery.of(context).size.width)
+      ? true
+      : false;
+}
